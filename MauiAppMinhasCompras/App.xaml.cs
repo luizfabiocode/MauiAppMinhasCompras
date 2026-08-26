185710507cc84029711412a595a46151ae34da9e
Luiz Fabio <luizfabiocamargo@gmail.com>
2026-08-25 21:26:18 -0300

    funcionalidade

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MauiAppMinhasCompras.Helpers;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQliteDatabaseHelper _db;
+
+        public static SQliteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_SQlite_compras.db3");
+
+
+                    _db = new SQliteDatabaseHelper(path);
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
@@ -11,10 +31,7 @@ namespace MauiAppMinhasCompras
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            // Comente ou remova esta linha se não quiser mais o AppShell
-            // return new Window(new AppShell());
-
-            // Use esta linha para iniciar com sua página ListaProduto
+            
             return new Window(new NavigationPage(new Views.ListaProduto()));
         }
     }
--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -4,6 +4,8 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        internal double Preco;
+
         [PrimaryKey, AutoIncrementAttribute]
         public int id { get; set; }
         public string Descricao { get; set; }
